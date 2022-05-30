dc55ca51935b29414e35b902df78f86695c76654
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-30 02:10:34 +0200

    Prepare for 0.08 version

--- Link.pm
+++ Link.pm
@@ -16,7 +16,7 @@ Readonly::Scalar our $COMMONS_URI => q{https://commons.wikimedia.org};
 Readonly::Array our @UPLOAD_SEGS => qw(wikipedia commons);
 Readonly::Array our @COMMONS_SEGS => qw(wiki);
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 sub new {
 	my ($class, @params) = @_;
@@ -326,6 +326,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
